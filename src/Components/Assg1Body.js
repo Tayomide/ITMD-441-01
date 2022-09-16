3b203609200c5fc38d6f4c4b73be851cd6f5bc0b
Tayomide <tibrahim@iltech.iit.edu>
2022-09-15 21:21:21 -0500

    Create Body component

---
+++ src/Components/Assg1Body.js
@@ -0,0 +1,58 @@
+import styled from "styled-components"
+import { Link } from  "react-router-dom"
+
+export const Assg1Body = () => {
+  return (
+    <Container>
+        <article>
+            <h1 className="hello">Hey!</h1>
+            <h2 className="name">I'm Tomiwa Ibrahim.</h2>
+            <h2 className="title">Student and Web developer</h2>
+            <p>I make websites for fun and I'm excited to be in a class that teaches me how to do just that!
+                You can check out my github for some cool projects I've worked on, but you're better off checking
+                my resume for completed projects, my <Link to="https://github.com/Tayomide">Github</Link> screams
+                commitment issues.</p>
+            <p>I love the fact that I can use any framework cos I've been warming up to React
+                for a while now and not being able to use it in my previous dev classes was no different from
+                torture. I'm really excited to be here and can't wait to see what I'll learn from this class!</p>
+            <button>Download CV</button>
+        </article>
+    </Container>
+  )
+}
+
+const Container = styled.section`
+    display:grid;
+    grid-template-columns: 9fr 8fr;
+    padding-top: 3em;
+    article{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        .hello, .title{
+            font-weight: 400;
+            font-size: 1.25em;
+            padding-bottom: 1em;
+            color: rgba(0,0,0,0.8);
+        }
+        .name{
+            color: #249de1;
+            font-size: 2.5em;
+            padding-bottom: 0.5em;
+        }
+        p{
+            font-size: 1.2em;
+            line-height: 1.25em;
+            padding-bottom: 1em;
+            word-spacing: 0.11em;
+        }
+        button{
+            background-color: #249de1;
+            border-radius: 0.3em;
+            color: #fff;
+            font-size: 1.1em;
+            padding: 0.8em 2em;
+            width: fit-content;
+        }
+    }
+`
