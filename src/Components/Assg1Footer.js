a3d41b7871f53b6f3d51128f668d4952baee3cbc
Tayomide <tibrahim@iltech.iit.edu>
2022-09-15 21:23:06 -0500

    Create structure for footer component

---
+++ src/Components/Assg1Footer.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+export const Assg1Footer = () => {
+  return (
+    <Footer>
+        <h4>Connect With me</h4>
+        <form>
+            <input placeholder="tayi@hawk.iit.edu"></input>
+            <input placeholder="Hey Tomiwa! Cool Assignment"></input>
+            <button>Stay Connected</button>
+        </form>
+    </Footer>
+  )
+}
+
+const Footer = styled.footer``
