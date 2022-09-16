60dfc1c1d6e40d15270e3a29bd209350afe1feb5
Tayomide <tibrahim@iltech.iit.edu>
2022-09-15 19:10:45 -0500

    Create and add header component to Assignment 1

---
+++ src/Assignments/Assg1.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+import { Assg1Header } from "../Components/Assg1Header"
+
+export const Assg1 = () => {
+  return (
+    <Container>
+      <Assg1Header />
+    </Container>
+  )
+}
+
+const Container = styled.section``
