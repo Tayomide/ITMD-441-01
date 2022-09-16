6a16cd0f88b88258302fc85d0e876eaaecab2b3b
Tayomide <tibrahim@iltech.iit.edu>
2022-09-15 21:23:27 -0500

    Add components to Assignmment 1

--- src/Assignments/Assg1.js
+++ src/Assignments/Assg1.js
@@ -1,12 +1,20 @@
 import styled from "styled-components"
 import { Assg1Header } from "../Components/Assg1Header"
+import { Assg1Body } from "../Components/Assg1Body"
+import { Assg1Interest } from "../Components/Assg1Interest"
 
 export const Assg1 = () => {
   return (
     <Container>
       <Assg1Header />
+      <main>
+        <Assg1Body />
+        <Assg1Interest />
+      </main>
     </Container>
   )
 }
 
-const Container = styled.section``
+const Container = styled.section`
+  padding: 1em 3em;
+`
