32d34f05a84a79f81ed2d1b78c001d049db7a2b6
Tayomide <tibrahim@iltech.iit.edu>
2022-09-15 21:20:54 -0500

    Add styling for header

--- src/Components/Assg1Header.js
+++ src/Components/Assg1Header.js
@@ -1,26 +1,26 @@
 import styled from "styled-components"
-import { Link } from 'react-router-dom'
-
+import { Link, NavLink } from 'react-router-dom'
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export const Assg1Header = () => {
   return (
     <Container>
-        <nav>
-            <Link to="https://github.com/Tayomide">Github</Link>
+        <nav className="github">
+            <Link to="https://github.com/Tayomide"><GitHubIcon /> Github</Link>
         </nav>
-        <nav>
+        <nav className="page-nav">
             <ul>
                 <li>
-                    <Link to="/">Home</Link>
+                    <NavLink to="/" className={isActive => (isActive ? "selected" : "")}>Home</NavLink>
                 </li>
-                <li>
-                    <Link to="/">Courses</Link>
+                <li className="disabled">
+                    <NavLink to="/" >Courses</NavLink>
                 </li>
-                <li>
-                    <Link to="/">About</Link>
+                <li className="disabled">
+                    <NavLink to="/">About</NavLink>
                 </li>
-                <li>
-                    <Link to="/">Contacts</Link>
+                <li  className="disabled">
+                    <NavLink to="/">Contacts</NavLink>
                 </li>
             </ul>
         </nav>
@@ -28,4 +28,45 @@ export const Assg1Header = () => {
   )
 }
 
-const Container = styled.header`` 
+const Container = styled.header`
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    justify-content: space-between;
+    a{
+        color: rgba(0,0,0,0.6);
+        font-weight: bold;
+        :hover{
+            color: #000
+        }
+    }
+    .github{
+        grid-column: 0 / span 3;
+        a{
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            svg{
+                margin-right: 0.2em;
+            }
+        }
+    }
+    .page-nav{
+        grid-column: 5 / -1;
+        ul{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            .disabled{
+                cursor: not-allowed;
+                a{
+                    pointer-events: none;
+                    color: rgba(0,0,0,0.5);
+                }
+                
+            }
+            a.selected{
+                color: #000;
+            }
+        }
+    }
+` 
